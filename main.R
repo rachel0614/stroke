b5f2bf46b451c620ffa4dee6362192ea8a7d7304
rachel <rachel0614@qq.com>
2021-05-16 08:05:52 +0100

    before submission

--- main.R
+++ main.R
@@ -34,18 +34,53 @@ dim(stroke_data)
 # see summary of the dataset
 summary(stroke_data)
 
+# according to the summary 
+# avg_glucose_level is between 55.12 and 271.74 in this dataset
+# we cut it into three level follow the rule :
+# normal = avg_glucose_level <= 140
+# prediabetes = avg_glucose_level <= 199
+# diabetes =  avg_glucose_level >199
+# create a new variable diabetes to indicate the diabetes status
+# categorical ordinal
+diabetes_col <- cut(stroke_data$avg_glucose_level,
+                    breaks = c(0, 140, 199, Inf), 
+                    labels = c('normal', 'prediabetes', "diabetes"), 
+                    right = FALSE,
+                    order = TRUE)
+stroke_data$diabetes <- diabetes_col
+
+# age level
+age_col <- cut(stroke_data$age,
+                    breaks = c(0, 30, 40, 50, 60, 70,Inf), 
+                    labels = c('under 30', '31-40', "41-50", "51-60","61-70","71 or older"), 
+                    right = FALSE,
+                    order = TRUE)
+stroke_data$age_level <- age_col
+
 
 # according to the summary, remove outliers of gender & smoking_status
 # and discard id & Date variables.
 stroke_data_modified <- subset(stroke_data, 
-                               select = c(age, bmi, 
+                               select = c(age, 
+                                          bmi, 
                                           smoking_status, 
-                                          avg_glucose_level, stroke), 
+                                          avg_glucose_level,
+                                          diabetes,
+                                          heart_disease,
+                                          ever_married,
+                                          age_level,
+                                          stroke), 
                                smoking_status != "Unknown")
 
 # create a new variable obesity with bmi>=30 is Yes else if No
 stroke_data_modified$obesity <- 
   factor(ifelse(stroke_data_modified$bmi>=30, "Yes", "No"))
+
+# create variable to indicate the patients smokes or not
+smoke_col <- factor(ifelse(stroke_data_modified$smoking_status == "smokes", 
+                           "Yes", "No"))
+stroke_data_modified$smoke <- smoke_col
+
 
 # drop the non-existed level of smoking_status factor 
 stroke_data_modified$smoking_status <- 
@@ -68,6 +103,8 @@ summary(stroke_data_modified)
 
 str(stroke_data_modified)
 dim(stroke_data_modified)
+data_backup <- stroke_data_modified
+# View(stroke_data_modified)
 ######################  data has been prepared##########
 
 # overview
@@ -88,18 +125,16 @@ pairs.panels(stroke_data_modified,
              ci = TRUE) 
 
 ########################################################
-# H0 - there is no relationship with smoking and stroke
-# H1 - there is relationship between smoking and stroke
-chisq.test(table(stroke_data_modified$smoking_level, 
-                 stroke_data_modified$stroke))
-str(stroke_data_modified)
-# p-value = 0.04906, accept H1
+# install.packages("ggplot2")
+# install.packages("ggpubr")
+library(ggplot2)
+library(ggpubr)
+library(scales)
+library(dplyr)
 ########################################################   
 attach(stroke_data_modified)
 
 # age hypothesis
-# H0 - age has no correlation with stroke 
-# H1 - age has correlation with stroke
 
 library("lattice")
 # boxplot to see data distribution in stroke and non-stroke patient group
@@ -107,7 +142,6 @@ plot(stroke, age, pch=9,col='lightblue',
      main = "stroke status vs age",
      ylab = "age",
      xlab = "group by stroke status")
-
 # compare age distribution for non-stroke and stroke patient group
 ggdensity(stroke_data_modified, 
           x = "age",
@@ -120,15 +154,10 @@ ggdensity(stroke_data_modified,
 # mean age in non-stroke and stroke group
 tapply(age, stroke, mean)
 
-# install.packages(histogram)
+# install.packages("histogram")
 library(histogram)
 # hist of age to check normality
 histogram(~age, data = stroke_data_modified,
-          main = "distribution of age data",
-          xlab = "age")
-
-# hist of age by stroke status to check normality
-histogram(~age | stroke, data = stroke_data_modified,
           main = "distribution of stroke & age data",
           xlab = "stroke status",
           ylab = "age")
@@ -163,37 +192,76 @@ normality_test$p.value
 tapply(age , stroke , summary)
 # stroke group = 68.06, non-stroke group 47.57
 
-# hypothesis test 
-# continuous independent variable - age
-# categorical variable - stroke (Yes/No)
-chisq.test(table(age, stroke))
 
+# hypothesis test 
+# categorical ordinal independent variable - age_level
+# categorical dichotomous variable - stroke (Yes/No)
+# H0 - age has no correlation with stroke
+# H1 = age has a correlation with stroke
+# check test data
+# chisq test
+
+# summary with row column total values
+age_table <- table(stroke, age_level)
+addmargins(age_table)
+
+barplot(prop.table(age_table,2)*100, 
+        ylab='Percentages',
+        main="percentage stroke by age groups", 
+        beside=F, 
+        col=c("#0073C2FF", "#FC4E07"),
+        legend=rownames(age_table), 
+        args.legend = list(x = "bottomleft"))
+
+chisq.test(age_table)
 # p-value < 2.2e-16, which is far smaller than 0.05
 # so we accept H1 that age has correlation with stroke
 
-# H0 - patient age in stroke and non-stroke patients group are similar
+# H0 - patient age in stroke and non-stroke patients group are equal
 # H1 - patient age in stroke and non-stroke patients group are significant different
 
 # independent continuous variable - age 
 # dependent categorical variable - stroke (Yes/No)
+# Mann-Withney-Wilcoxon test (use same function with wilcox)
 wilcox.test(age ~  stroke)
+# wilcox.test(age ~  stroke, alternative="less")
+
 # p-value < 2.2e-16, at 0.05 significance level, we conclude that 
 # age of stroke and non-stroke patients are significantly different.
 
+# smoking hypothesis 
 
-# from the above boxplot showing patient age by stroke status,
-# It seems the mean age of stroke patient is higher than that of the stroke group 
-# this can be tested by adding alternative = "less" 
-# we keep doing on-tailed hypothesis test as below
+# H0 - smoking status has correlation with stroke 
+# H1 - smoking status has correlation with stroke
+chisq.test(table(smoking_status, stroke))
+# p-value = 0.04996
+# stroke group data 
+smoking_stroke_data <- stroke_data_modified[stroke == "Yes", ]
+# plot age of stroke patient by smoking status
+qplot(x = smoking_status, 
+      y = age,
+      geom = "boxplot", 
+      data = smoking_stroke_data,
+      xlab = "smoking status", 
+      ylab = "age",
+      fill = I("lightblue"))
 
-# H0 - age in stroke group is higher than non-stroke group
-# H1 - age in stroke group is not higher than non-stroke group
-wilcox.test(age ~  stroke,
-            alternative = "less")
+# check distribution shape
+histogram(~age | smoke , data = smoking_stroke_data,
+          main = "distribution of smoking status & age",
+          xlab = "smoking status",
+          ylab = "age")
 
-# p-value < 2.2e-16, at 0.05 significance level, we conclude that 
-# age of stroke patient is significantly higher than non-stroke group.
+# mean age in stroke patients by smoking status
+tapply(smoking_stroke_data$age , smoking_stroke_data$smoking_status , summary)
 
+# H0 - age is equal in three smoking status group of the stroke patients
+# H1 - age is equal in three smoking status group of the stroke patients 
+# age - continuous 
+# smoking - categorical dichotomous
+wilcox.test(smoking_stroke_data$age ~  smoking_stroke_data$smoke)
+
+# p-value = 0.002014
 # average glucose level hypothesis
 # 
 # plot the distribution of patients by average glucose level
@@ -205,7 +273,7 @@ ggdensity(stroke_data_modified, x = "avg_glucose_level",
           palette = c("#0073C2FF", "#FC4E07"),
           xlab = "aver glucose level")
 
-# glucose level under 120 distributed fewer stroke patients 
+# it seems the glucose level under 120 distributed fewer stroke patients 
 # than that of  above 120
 
 # use Q-Q plot to check if average glucose level is normally distributed
@@ -220,16 +288,9 @@ histogram(~avg_glucose_level | stroke, data = stroke_data_modified,
           xlab = "stroke status",
           ylab = "average glucose level")
 
-
-# group
-with(stroke_data_modified, { 
-  qqnorm(avg_glucose_level[stroke == 'Yes'], main = 'stroke data')  
-  qqline(avg_glucose_level[stroke == 'Yes'], col = "red")
-  })
-
 qplot(sample = avg_glucose_level,
       data = stroke_data_modified,
-      color = stroke) + theme(legend.position="bottom")
+      color = stroke) + theme(legend.position="right")
 # both stroke and non-stroke not distributed in a straight line 
 # it's unlike normally distributed
 
@@ -245,86 +306,34 @@ normality_test$p.value
 # p-value = 2.212745e-53
 # avg_glucose_level is not normally distributed 
 
-# hypothesis test 
-# continuous independent variable - avg_glucose_level
-# categorical variable - stroke (Yes/No)
-chisq.test(table(avg_glucose_level, stroke))
-
-# BMI hypothesis
-#
-with(stroke_data_modified,
-     qqplot(bmi[stroke == "Yes"],
-            bmi[stroke == "No"],
-            main = "compare 2 samples of patients data",
-            xlab = "stroke = Yes",
-            ylab = "stroke = No"))
-# the line is quite straight except one outlier
-
-# 
+# bmi 
 with(stroke_data_modified, { 
-  qqnorm(bmi[stroke == 'Yes'], main = 'Stroke patients')  
-  qqline(bmi[stroke == 'Yes']) 
+  qqnorm(bmi, main = 'bmi')  
+  qqline(bmi) 
 })
-
-with(stroke_data_modified, { 
-  qqnorm(age[stroke == 'No'], main = 'Stroke patients')  
-  qqline(age[stroke == 'No']) 
-})
-
-normality_test <- shapiro.test(stroke_data_modified$bmi)
-normality_test$p.value
-# p-value = 2.920174e-35 
-# bmi is normally distributed
-
-# H0 - bmi has no correlation with stroke
-# H1 - bmi has correlation with stroke
-chisq.test(table(bmi, stroke))
 
 normality_test <- shapiro.test(bmi)
 normality_test$p.value
+# p-value = 2.855762e-35 not normally distributed 
 
-cor.test(avg_glucose_level
-         , bmi, 
+# avg_glucose_level hypothesis
+ggqqplot(stroke_data_modified$avg_glucose_level, 
+         ylab = "avg_glucose_level")
+
+shapiro.test(stroke_data_modified$avg_glucose_level)
+
+# correlation between avg_glucose_level & bmi
+# H0 - there is no correlation between avg_glucose_level & bmi
+# H1 - ther is correlation between avg_glucose_level & bmi
+data <- stroke_data_modified[stroke=="Yes", ]
+cor.test(data$avg_glucose_level,
+         data$bmi,
          method = "spearman")
-
-data <- stroke_data_modified[stroke == "Yes", ]
+# stroke group p-value = 6.81e-05 rho = 0.2923862
+data <- stroke_data_modified[stroke=="No", ]
+cor.test(data$avg_glucose_level,
+         data$bmi,
+         method = "spearman")
+# non-stroke group p-value = 2.669e-08, rho=0.09
 detach(stroke_data_modified)
-
-attach(data)
-# smoking status
-# plot age of stroke patient by smoking status
-qplot(x = smoking_status, 
-      y = age,
-      geom = "boxplot", 
-      data = data,
-      xlab = "smoking status", 
-      ylab = "age",
-      fill = I("lightblue"))
-
-
-# mean age in stroke patients by smoking status
-tapply(data$age , data$smoking_status , summary)
-
-ggdensity(data, x = "age",
-          add = "mean", 
-          rug = TRUE,
-          title = "age distribution \n   by smoking status in stroke patients",
-          color = "smoking_status", fill = "smoking_status",
-          palette = c("#0073C2FF", "#FC4E07", "#FF0000"),
-          xlab = "age")
-# it looks more older non-smokers, smokers tends to be younger stroke patients 
-
-# independent continuous age
-# independent categorical ordinal smoking level
-# H0 - smoking has no correlation with mean age of stroke patient
-# H1 - smoking has correlation with mean age of stroke patient
-cor.test(data$age, 
-         as.numeric(smoking_status), 
-         exact = FALSE, 
-         method = "spearman")
-
-# p-value = 0.0009329 reject H0 which means that 
-# smoking has correlation with mean age of stroke patient
-# rho = -0.244659, it shows that 
-# age of stroke has negative correlation with smoking level
-
+str(stroke_data_modified)
